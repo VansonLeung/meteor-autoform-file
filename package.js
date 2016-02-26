ac264ccf9afeb0a91f9f6ca64afdad7090974a5e
Vanson <vanson@vanportdev.com>
2016-02-26 19:08:32 +0800

    Update package

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
-  name: "yogiben:autoform-file",
+  name: "vansonhk:autoform-file",
   summary: "File upload for AutoForm",
   description: "File upload for AutoForm",
   version: "0.4.2",
-  git: "https://github.com/yogiben/autoform-file.git"
+  git: "https://github.com/VansonLeung/autoform-file.git"
 });
 
 Package.onUse(function(api) {
